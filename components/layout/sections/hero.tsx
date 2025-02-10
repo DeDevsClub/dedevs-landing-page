99fb258850bf1ec6f4da15c91c30b918e01dd664
Buns Enchantress <68980965+BunsDev@users.noreply.github.com>
2025-02-10 03:07:06 -0600

    Update hero.tsx

--- components/layout/sections/hero.tsx
+++ components/layout/sections/hero.tsx
@@ -52,7 +52,7 @@ export const HeroSection = () => {
 
 			<div className="relative group bg-primary -mt-[128px] w-full">
 				<div
-					className="absolute top-2 lg:-top-8 left-1/2 transform 
+					className="hidden sm:absolute top-2 lg:-top-8 left-1/2 transform 
             -translate-x-1/2 w-[90%] mx-auto h-24 lg:h-80 bg-primary/50 rounded-full blur-3xl"
 				/>
 				<Image
@@ -62,7 +62,7 @@ export const HeroSection = () => {
 					// className="w-full md:w-[1280px] mx-auto rounded-lg relative rouded-lg leading-none flex items-center border border-t-2 border-primary  border-t-primary/30"
           // rounded-t-[20px] 
 					className={`
-              hidden sm:w-screen h-[48rem] flex items-center
+               w-screen h-[48rem] flex items-center
               justify-center border-0 border-primary/100
             `}
 					src={
@@ -76,29 +76,7 @@ export const HeroSection = () => {
 					height={1200}
 					alt="dashboard"
 				/>
-				<Image
-					// width={2560}
-					// height={2560}
-					// className="w-full md:w-[1280px] mx-auto rounded-lg relative rouded-lg leading-none flex items-center border border-t-2 border-primary  border-t-primary/30"
-					// className="w-full md:w-[1280px] mx-auto rounded-lg relative rouded-lg leading-none flex items-center border border-t-2 border-primary  border-t-primary/30"
-          // rounded-t-[20px] 
-					className={`
-              w-screen sm:hidden h-[48rem] flex items-center
-              justify-center border-0 border-primary/100
-            `}
-					src={
-						"/logo.png"
-						// "/hero-image.png"
-						// theme === "light"
-						//   ? "/hero-image-light.jpeg"
-						//   : "/hero-image-dark.jpeg"
-					}
-					width={1800}
-					height={1200}
-					alt="dashboard"
-				/>
 			</div>
-
         <div
             className="flex justify-center items-center text-center h-full text-2xl p-2 bg-primary/100
                 border-primary/100 font-nippo w-full rounded-b-[20px] hover:animate-pulse mx-auto"
